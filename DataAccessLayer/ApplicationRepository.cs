e8271713e1d93b3410ae47f73a3a070636959212
ayarnykh <ayarnykh@rustest.ru>
2023-12-12 16:29:20 +0300

    Modifying repository.

---
+++ DataAccessLayer/ApplicationRepository.cs
@@ -0,0 +1,52 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ApplicationRepository : IApplicationRepository, IRepository<Application>
+    {
+        public Task Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Application>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<Application>> GetAllApplications()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<Application>> GetAllApplicationsById(int ApplicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Application> GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Application> Insert(Application Element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Application> Update(Application Element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/IApplicationRepository.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLayer.Entities;
+
+namespace DataAccessLayer
+{
+    public interface IApplicationRepository : IRepository<Application>
+    {
+        public Task<IQueryable<Application>> GetAllApplications();
+        public Task<IQueryable<Application>> GetAllApplicationsById(int ApplicationId);
+    }
+}
