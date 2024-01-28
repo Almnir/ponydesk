d747c76e7d1b26f756de0ef40122a575d8e1b3d3
Almnir <almnir.mail@gmail.com>
2024-01-28 23:12:25 +0300

    Minor fixes.

--- DataAccessLayer/Repository.cs
+++ DataAccessLayer/Repository.cs
@@ -94,23 +94,12 @@ namespace DataAccessLayer
                 {
                     _context.Dispose();
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
                 disposedValue = true;
             }
         }
 
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~Repository()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
-
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
